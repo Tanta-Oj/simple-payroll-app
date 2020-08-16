cb014863e73a8b5c5c4dbbb1951bbe65a97e8992
Kenta Tanaka <tantapg200309@gmail.com>
2020-08-16 21:47:22 +0900

    deviseによるログイン機能の実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: '192.168.10.3', port: 3000 }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      enable_starttls_auto: true,
+      address: "smtp.gmail.com",
+      port: 587,
+      user_name: ENV['GMAIL_USERNAME'],
+      password: ENV['GMAIL_PASSWORD'],
+      authentication: "plain"
+  }
 
   config.action_mailer.perform_caching = false
 
